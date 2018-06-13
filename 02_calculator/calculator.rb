4f01898b8d7c8084153e20f808409cf266be6873
Dennis <dlrenner@yahoo.com>
2018-06-13 16:40:33 -0500

    Calculator changes

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def sum(arr)
+    sum = 0
+    arr.each do |i|
+        sum = sum + i
+    end
+    sum
+end
+
+def multiply(arr)
+    product = 1
+    arr.each do |i|
+        product = product * i
+    end
+    product
+end
+
+def power(num, power)
+    num ** power
+end
+
+def factorial(num)
+    product = 1
+
+    num.times do |i|
+        product = product * (i + 1) # .times starts at 0
+    end
+    return product
+end
