82e88a2221fe628dea689494b28784c9d3c0fde8
Dennis <dlrenner@yahoo.com>
2018-06-14 01:11:10 -0500

    Completed Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
 #write your code here
+def echo(str)
+    str
+end
+
+def shout(str)
+    str.upcase
+end
+
+def repeat(str, num = 2)
+    returnStr = ""
+
+    num.times do
+        returnStr = returnStr + str + " "
+    end
+
+    returnStr.rstrip
+end
+
+def start_of_word(word, index)
+    word[0, index]
+end
+
+def first_word(str)
+    str.split[0]
+end
+
+def titleize title
+    little_words = ["a", "and", "the", "of", "over"]
+
+    words = title.split
+    words.each do |word|
+        if not little_words.include?(word)
+            word.capitalize!
+        end
+    end
+    words[0].capitalize!
+
+    words.join(" ")
+end
