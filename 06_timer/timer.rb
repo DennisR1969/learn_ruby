36e1315393912c6a9b2a7fa9bf3cbb1089d679ff
Dennis <dlrenner@yahoo.com>
2018-06-14 16:57:52 -0500

    finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    minutes = @seconds / 60
+    hours = minutes / 60
+
+    @seconds = @seconds % 60
+    minutes = minutes % 60
+
+    return formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(@seconds)
+  end
+
+  def formatTime(time)
+    return (time < 10 ? "0" : "") + time.to_s
+  end
+
+  def seconds=(sec)
+    @seconds = sec
+  end
+
+  def seconds
+    @seconds
+  end
 end
