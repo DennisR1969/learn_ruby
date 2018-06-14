172623f187c1f01902091e9126cd3560fcd4d350
Dennis <dlrenner@yahoo.com>
2018-06-14 16:29:37 -0500

    finished book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
 # write your code here
+  @title = ""
+
+  def title=(title)
+    little_words = ["a", "an", "and", "in", "of", "the"]
+    words = title.split
+
+    words.each do |word|
+        if not little_words.include?(word)
+            word = word.capitalize!
+        end
+    end
+
+    words.first.capitalize!
+    @title = words.join(" ")
+  end
+
+  def title
+    @title
+  end
+
 end
